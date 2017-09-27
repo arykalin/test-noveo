415dea85f1a31dfc55d47042fb07abc01c81f59b
Aleksandr Rykalin <arykalin@luxoft.com>
2017-09-27 19:56:11 +0300

    Adding examples

--- server.py
+++ server.py
@@ -32,5 +32,22 @@ sock.listen(1)
 
 while True:
     # Wait for a connection
-    print ('waiting for a connection')
+    print('waiting for a connection')
     connection, client_address = sock.accept()
+    try:
+        print('connection from', client_address)
+
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(16)
+            print('received "%s"' % data)
+            if data:
+                print('sending data back to the client')
+                connection.sendall(data)
+            else:
+                print('no more data from', client_address)
+                break
+
+    finally:
+        # Clean up the connection
+        connection.close()
